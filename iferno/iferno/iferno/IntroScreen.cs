4a244de67e135f3a10d939063968e3e2a627f1c6
Orangen <Orangen@users.noreply.github.com>
2013-07-30 12:01:55 +0200

    screens

---
+++ iferno/iferno/iferno/IntroScreen.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace iferno
+{
+    public class IntroScreen : Screen
+    {
+        public IntroScreen(Game1 game, SpriteBatch spriteBatch)
+            : base(game, spriteBatch)
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}

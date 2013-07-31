1590f3fe4a0be5bc559cc51b8359334d26cbb9ed
Iferno <attilaatlantis@yahoo.de>
2013-07-31 10:21:22 +0200

    MenuScreen mit leben gefüllt

--- iferno/iferno/iferno/Screens/MenuScreen.cs
+++ iferno/iferno/iferno/Screens/MenuScreen.cs
@@ -14,32 +14,63 @@ namespace iferno
 {
     public class MenuScreen : Screen
     {
-        KeyboardState oldKeyboardState = Keyboard.GetState();
-
+        int indexPos = 0;
         public MenuScreen(Game1 game, SpriteBatch spriteBatch)
             : base(game, spriteBatch)
         {
 
         }
 
-        public override void activate()
-        {
-            oldKeyboardState = Keyboard.GetState();
-        }
-
         public override void LoadContent()
         {
-
+            Settings.Textures.Add("textGameSound", Content.Load<Texture2D>("textGameSound"));
+            Settings.Textures.Add("textGameWeiter", Content.Load<Texture2D>("textGameWeiter"));
         }
 
         public override void Update(GameTime gameTime)
         {
+            KeyboardState menuState = Keyboard.GetState();
+
+            if(menuState.IsKeyDown(Keys.Up) && indexPos > 0)
+            {
+                indexPos--;
+            }
+            else if (menuState.IsKeyDown(Keys.Down) && indexPos < 3)
+            {
+                indexPos++;
+            }
+
+            if (menuState.IsKeyDown(Keys.Enter))
+            {
+                switch (indexPos)
+                { 
+                    case 0:
+                        game.switchScreen("level");
+                        break;
+                    case 1:
+                        game.switchScreen("level");
+                        break;
+                    case 2:
+                        //Sound an und ausschalten
+                        break;
+                    case 3:
+                        game.Exit();
+                        break;
+                }
+            }
 
         }
 
         public override void Draw(GameTime gameTime)
         {
-
+            //Weiter
+            spriteBatch.Draw(Settings.Textures["textGameWeiter"], new Vector2(750, 100), indexPos == 0 ? Color.White : Color.DimGray);
+            //New Game
+            spriteBatch.Draw(Settings.Textures["textGameStarten"], new Vector2(750, 200), indexPos == 1 ? Color.White : Color.DimGray);
+            //Sound
+            spriteBatch.Draw(Settings.Textures["textGameSound"], new Vector2(750, 300), indexPos == 2 ? Color.White : Color.DimGray);
+            //Exit
+            spriteBatch.Draw(Settings.Textures["textGameBeenden"], new Vector2(750, 400), indexPos == 3 ? Color.White : Color.DimGray);
         }
     }
 }

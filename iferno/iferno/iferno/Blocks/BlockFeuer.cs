c09c1baf2f66584707beccaf9356cbae07412823
jens <jens>
2013-08-02 10:41:50 +0200

    collision from fire

--- iferno/iferno/iferno/Blocks/BlockFeuer.cs
+++ iferno/iferno/iferno/Blocks/BlockFeuer.cs
@@ -17,6 +17,14 @@ namespace iferno
             collideWithPlayer = false;
         }
 
+        public override Rectangle Collision()
+        {
+            Rectangle collision = base.Collision();
+            collision.Y = collision.Y + (collision.Height * 2 / 3);
+            collision.Height = collision.Height * 1 / 3;
+            return collision;
+        }
+
         public override void OnCollisionWithPlayer(Player p)
         {
             Settings.SoundEffects["pickup"].Play();
